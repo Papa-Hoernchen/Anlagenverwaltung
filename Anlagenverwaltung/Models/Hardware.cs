6a6f4fec8c537da58310d99a31e4c53b14733eb7
wiens1612 <wiens1612@gmx.de>
2020-03-11 12:45:31 +0100

    Projekt Anlagenverwaltung angelegt.

---
+++ Anlagenverwaltung/Models/Hardware.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Anlagenverwaltung.Models
+{
+    public class Hardware
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
